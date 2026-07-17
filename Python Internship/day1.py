65d93decb9c062c5cadb2a12a90d9ea185c6a030
mayurunagar5 <mayurunagar555@gmail.com>
2026-07-17 17:09:22 +0530

    Latest

--- Python Internship/day1.py
+++ Python Internship/day1.py
@@ -9,7 +9,7 @@ else:
 
 
 #operators
-
+#1 Arithmatic 
 a = 10
 b = 3
 
@@ -20,6 +20,62 @@ print(a / b)
 print(a // b)
 print(a % b)
 print(a ** b)
+
+#2 Assignment 
 c = 5
 c += 10
 print(c)
+
+#3 comparison
+# 
+d= 10
+
+print(a==b)
+print(a!=c)
+
+#4 Logical
+
+print(a!=b and a==d)
+print(True or False)
+
+#5 Bitwise Operators
+
+print("AND :", a & b)
+print("OR  :", a | b)
+print("XOR :", a ^ b)
+print("NOT :", ~a)
+print("LEFT SHIFT :", a << 1)
+print("RIGHT SHIFT:", a >> 1)
+
+#6 membership operator
+
+car = ["BMW", "porsche", "bently"]
+print("BMW" in car)
+
+#7 Identity operator
+
+num = [1 ,2]
+N = num
+print(N is num)
+print(num is N)
+
+# Name1 = input("Enter your name:")
+# print("good evening,", Name1)
+
+
+# age = input("Enter age: ")
+# print("your age, ", age)
+# print(type(age))
+
+# Type Casting Example
+
+# year= int(input("Enter Any year: "))
+# print(year + 1000)
+
+# new= float("99.99")
+# print(new)
+
+# new1 = 100
+# print(str(new1))
+
+
