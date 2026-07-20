60e0fbf36521a8b2df16889a0043d4feb07d7657
mayurunagar5 <mayurunagar555@gmail.com>
2026-07-20 12:32:53 +0530

    New code added

---
+++ Python Internship/DAY2/Code.py
@@ -0,0 +1,25 @@
+from pydoc import text
+
+# String Operations
+
+name = "Mayur Unagar"
+Field = "IT"
+print(name[2])
+print(name[1:5])
+print(name[:])
+print(name[:7])
+print(name[0:9:2])
+
+print(name+" "+Field)
+
+print(Field * 3)
+
+print("unagar" in name)
+
+print(len(name))
+
+print("Mayur" == "mayur")
+
+for ch in name:
+    print(ch)
+
